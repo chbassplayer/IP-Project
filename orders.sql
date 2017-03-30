ccd0a2f3c135d9df82c43ffa319d0aa7c934ed66
chbassplayer <jessica-lu@uiowa.edu>
2017-03-30 01:57:01 -0500

    orders

--- orders.sql
+++ orders.sql
@@ -1,5 +1,5 @@
 --not an itemized copy--
-drop table =u7-67-==if exists orders;
+drop table if exists orders;
 
 create table orders(
     id INT NOT NULL AUTO_INCREMENT,
@@ -8,7 +8,8 @@ create table orders(
     orderDate date NOT NULL,
     orderStatus int NOT NULL,
     preferredDate date NOT NULL,
-    preferredTime time NOT NULL,
-    cartID int NOT NULL,
+    preferredTime1 int NOT NULL,
+    preferredTime2 int NOT NULL,
     PRIMARY KEY (id)
 );
+INSERT INTO orders(customerID,storeID,orderDate,orderStatus,preferredDate,preferredTime1,preferredTime2)VALUES(1234,4321,'17-02-17',0,'17-02-19',6,7);
