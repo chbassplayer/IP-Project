c65d959d62f42e152c72b9563fcedd9ff9fc2d7e
Dominic Fosco <dominic-fosco@uiowa.edu>
2017-04-04 05:39:59 -0500

    customer help section with contact info

---
+++ customerHelp.php
@@ -0,0 +1,8 @@
+<?php
+    include_once('customerHome.html');
+?>
+
+<h1>For assistance please email: <span class="label label-info"> <span class=" glyphicon glyphicon-envelope"></span> dominic-fosco@uiowa.edu</span></h1>
+<h1>For assistance please email: <span class="label label-info"> <span class=" glyphicon glyphicon-envelope"></span> jessica-lu@uiowa.edu</span></h1>
+
+<h1>For immediate assistance please call: <span class="label label-info"> <span class=" glyphicon glyphicon-earphone"></span> (888) 123-678</span></h1>
