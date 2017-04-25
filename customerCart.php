c293ee8ca7fc9140eadb8cf14874ee6ca76b7363
chbassplayer <jessica-lu@uiowa.edu>
2017-04-25 09:28:49 -0500

    some buttons

--- customerCart.php
+++ customerCart.php
@@ -19,74 +19,114 @@
 </style>
 </head>
 <body>
+<div class="heading">
+		<h1 style="color:gray">Store Name</h1>
+</div>
     <?php
     include_once('config.php');
     include_once('dbutils.php');
     session_start();
     $storeID=$_SESSION['StoreID'];
     ?>
-		<nav class="navbar navbar-inverse">
+    <nav class="navbar navbar-inverse">
                  <div class="container-fluid">
                    <div class="navbar-header">
-                     <a class="navbar-brand" href="customerHome.php">Store Name</a>
+                     <a class="navbar-brand" href="customerHomeForEdits.php">Store Name</a>
                    </div>
 				   
-				<ul class="nav navbar-nav">
-                                                            
-  <div class="dropdown" style="padding-top:8%">
-    <button class="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown">Categories
-    <span class="caret"></span></button>
-    <ul class='dropdown-menu'>;
-       <?php    
-    include_once('config.php');
-    include_once('dbutils.php');
-    $storeID=1;
-    $db = connectDB($DBHost, $DBUser, $DBPasswd, $DBName);
-    $query="SELECT * from categories where StoreID=$storeID;";
-    $result = queryDB($query, $db);
-    while($row = nextTuple($result)) {
-        $categoryID=$row['id'];
-        $catName=$row['catName'];
-            echo "<li class='dropdown-submenu'>";
-                echo "<a class='test' tabindex='-1' href='#'>$catName<span class='caret'></span></a>";
-                echo "<ul class ='dropdown-menu'>";
-                $query1="SELECT subName from SubCats where StoreID=$storeID and MainCatID=$categoryID;";
-                $result1 = queryDB($query1, $db);
-                while($row1 = nextTuple($result1)) {
-                $subName=$row1['subName'];
-                
-                    echo"<li><a tabindex='-1' href='#'>$subName</a></li>";
-                }
-                echo"</ul>";
-                echo "</li>";
-    }
-        ?>
-    
-    </ul>
-  </div>
-
-                </ul>
+				   <ul class="nav navbar-nav navbar-right">
                    
-                   <ul class="nav navbar-nav navbar-right">
-                        <li><a href="customer-logout.php"><?php echo $_SESSION['email']; ?><span class="glyphicon glyphicon-user"></span></a></li>
+                        <li style ="padding-top:2%; padding-bottom:1%">
+                        <form method="post" action="customerHomeForEdits.php">
+                            <input type="text" value="Search..." name="query" />
+                            <input type="submit" value="Find" name="completedsearch" />
+                        </form>
+                        </li>
+
 						<li><a href="customerCart.php">Cart <span class="glyphicon glyphicon-shopping-cart"></span></a></li>
                         <li><a href="customerSettings.php">Settings <span class="glyphicon glyphicon-cog"></span></a></li>
                         <li><a href="customerHelp.php">Help <span class="glyphicon glyphicon-question-sign"></span></a></li>
+                        <?php
+                        if($_SESSION['email']==""){
+                            echo "<li><a href='login-customer.php'>Login<span class='glyphicon glyphicon-user'></span></a></li>";
+                        }
+                        else{
+                            echo "<li><a href='customer-logout.php'>Logout:". $_SESSION['email']."<span class='glyphicon glyphicon-user'></span></a></li>";
+                        }
+                        ?>
+						
                    </ul>
-                   <a class="btn btn-default" href="draftCustomerLogout.php" style="position:absolute; top:0; right:0;">Log Out <b></b></a>
+				   
+				   
+				   
+				   
+				   
+				   
+				   
+			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+				<ul class="nav navbar-nav">
 
-                 
-                 
-                </div>
+					<li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Categories <span class="caret"></span></a>
+					<ul class="dropdown-menu">
+                            <?php    
+                        $db = connectDB($DBHost, $DBUser, $DBPasswd, $DBName);
+                        $query="SELECT * from categories where StoreID=$storeID order by catName;";
+                        $result = queryDB($query, $db);
+                        while($row = nextTuple($result)) {
+                            $categoryID=$row['id'];
+                            $catName=$row['catName'];
+				echo "<li class='dropdown-submenu'>";
+					echo "<a class='test' tabindex='-1' href='customerShowBigCategory.php?id=$categoryID'>$catName<span class='caret'></span></a>";
+					echo "<ul class ='dropdown-menu'>";
+					$query1="SELECT id,subName from SubCats where StoreID=$storeID and MainCatID=$categoryID;";
+					$result1 = queryDB($query1, $db);
+                    echo  "<li><a tabindex='-1' href='customerShowBigCategory.php?id=$categoryID'>All $catName</a></li>";
+					while($row1 = nextTuple($result1)) {
+					$subName=$row1['subName'];
+                    $subID=$row1['id'];
+								
+						echo"<li><a tabindex='-1' href='customerItemsViewForEdits.php?id=$subID'>$subName</a></li>";
+					
+					}
+					echo"</ul>";
+					echo "</li>";
+					}
+                        ?>
+				   </div>
+				   
+				   
+				   
+				   
+				   
+				</div>
 
                  
             </nav>
-  <table class="table table-hover">
+
+<script>
+$(document).ready(function(){
+  $('.dropdown-submenu a.test').on("click", function(e){
+    $(this).next('ul').toggle();
+    e.stopPropagation();
+    e.preventDefault();
+  });
+});
+</script>
+<div class ="row">
+    <div class="col-xs-12">
+    <h1>Your Cart</h1>
+    </div>
+</div>
+<div class="row">
+    <div class="col" style ="padding:3%">
+    <table class="table table-hover">
     <!-- headers for table -->
-    <thead>
+    <thead style ="padding:5%">
         <th>Name</th>
         <th>Brand</th>
         <th>Quantity</th>
+        <th></th>
+        <th></th>
         <th>Price</th>
         
     </thead>
@@ -97,42 +137,69 @@
     
     $storeID=$_SESSION['StoreID'];
     $order=$_SESSION['startedOrder'];
+    if($order==""){
+        echo "Currently No Items In Cart";
+        exit;
+    }
     $db = connectDB($DBHost, $DBUser, $DBPasswd, $DBName);
     //get Date
-    $query="SELECT Nam,Brand,quantityInOrder,Price from itemsInOrder join items on itemsInOrder.itemID=items.ID where StoreID=$storeID
+    $query="SELECT Nam,Brand,quantityInOrder,Price,items.ID,image from itemsInOrder join items on itemsInOrder.itemID=items.ID where StoreID=$storeID
     and orderID=$order;";
     $result = queryDB($query, $db);
     while($row = nextTuple($result)) {
-        
+        $itemID=$row['ID'];
         $Name=$row['Nam'];
         $Brand=$row['Brand'];
         $quantity=$row['quantityInOrder'];
         echo "\n <tr>";
         echo "<td>" . $Name. "</td>";
         echo "<td>" . $Brand . "</td>";
-        echo "<td>" . $quantity . "</td>";
+        echo "<td><form action='UpdateCart.php?id=$itemID' method='post'>";
+        echo "<div class='form-inline'>";
+        //echo "<label for='Quantity'>Quantity:</label>";
+        echo "<input type='number' class='form-control' name='Quantity' style= width:15% value='$quantity'/>";
+        echo "</div>";
+        echo "<td>";
+         if ($_SESSION['startedOrder']){
+            echo "<button type='submit' class='btn btn-default' name='UpdateCart$itemID'>Update My Cart</button>";
+            echo "</td>";
+        }
+        echo "</form></td>";
+        echo "<td>";
+        if($row['image']){
+            $imageLocation=$row['image'];
+            echo "<img src=$imageLocation width='70'>";
+        }
+
+        echo "</td>";
         echo "<td>" . $row['Price'] . "</td>";
         echo "\n <tr>";
+    }
             
-        }
         
-
-
-    
+       
+        
     ?>
+
     </table>
-    <a class="btn btn-default" href="checkingOut0.php">Check Out <b></b></a>
-<script>
-$(document).ready(function(){
-  $('.dropdown-submenu a.test').on("click", function(e){
-    $(this).next('ul').toggle();
-    e.stopPropagation();
-    e.preventDefault();
-  });
-});
-</script>
-
-
+    <?php
+    $db = connectDB($DBHost, $DBUser, $DBPasswd, $DBName);
+    $queryTotal="SELECT sum(quantityInOrder*Price) as Total from itemsInOrder,items where items.id=itemsInOrder.itemID
+    and OrderID=$order and StoreID=$storeID;";
+    $resultTotal=queryDB($queryTotal,$db);
+    while($row=nextTuple($resultTotal)){
+        $Total=$row['Total'];
+    }
+     
+    ?>
+        </div>
+    </div>
+    <div class="row">
+        <div class= "col-xs-12" style="position:absolute; right:5%;">
+        <p  style="position:absolute; right:0%;"><b>Total </b><?php echo $Total;?></p>
+        <p><a class="btn btn-default" href="checkingOut0.php" style="position:absolute; right:7%;">Check Out <b></b></a></p>
+        </div>
+    </div>
 
 </body>
 </html>
