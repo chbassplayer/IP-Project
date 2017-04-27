43c16bb96022bac7c36042b61c15ce5b9c10dd4c
chbassplayer <jessica-lu@uiowa.edu>
2017-04-27 01:05:15 -0500

    ok

--- customer-logout.php
+++ customer-logout.php
@@ -2,12 +2,12 @@
     // log user out by unsetting session variable called email, and destroying the session
     
     session_start();
-    if (isset($_SESSION['email'])) {
-        unset($_SESSION['email']);
+    if (isset($_SESSION['Cemail'])) {
+        unset($_SESSION['Cemail']);
     }
     session_destroy();
     
     // redirect user to login page
-    header("Location: login-customer.php");
+    header("Location: customerHomeForEdits.php");
     exit;
 ?>
