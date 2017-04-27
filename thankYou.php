0d2a0a1cef679a62e482687ac323be7b375b96dd
chbassplayer <jessica-lu@uiowa.edu>
2017-04-27 01:11:09 -0500

    ok

--- thankYou.php
+++ thankYou.php
@@ -29,65 +29,85 @@
 </head>
 <body>
 
-     <nav class="navbar navbar-inverse">
-                 <div class="container-fluid">
-                   <div class="navbar-header">
-                     <a class="navbar-brand" href="customerHome.php">Store Name</a>
-                   </div>
-				   
-				<ul class="nav navbar-nav">
-                                                            
-  <div class="dropdown" style="padding-top:8%">
-    <button class="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown">Categories
-    <span class="caret"></span></button>
-    <ul class='dropdown-menu'>;
-       <?php    
-    include_once('config.php');
-    include_once('dbutils.php');
-    $storeID=1;
-    $db = connectDB($DBHost, $DBUser, $DBPasswd, $DBName);
-    $query="SELECT * from categories where StoreID=$storeID;";
-    $result = queryDB($query, $db);
-    while($row = nextTuple($result)) {
-        $categoryID=$row['id'];
-        $catName=$row['catName'];
-            echo "<li class='dropdown-submenu'>";
-                echo "<a class='test' tabindex='-1' href='#'>$catName<span class='caret'></span></a>";
-                echo "<ul class ='dropdown-menu'>";
-                $query1="SELECT subName from SubCats where StoreID=$storeID and MainCatID=$categoryID;";
-                $result1 = queryDB($query1, $db);
-                while($row1 = nextTuple($result1)) {
-                $subName=$row1['subName'];
-                
-                    echo"<li><a tabindex='-1' href='#'>$subName</a></li>";
-                }
-                echo"</ul>";
-                echo "</li>";
-    }
-        ?>
-    
-    </ul>
-   
-  </div>
-      
+    <div class="heading">
+		<h1 style="color:gray">Store Name</h1>
+</div>
 
+<nav class="navbar navbar-inverse">
+        <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="customerHomeForEdits.php">Store Name</a>
+        </div>
+        
+        <ul class="nav navbar-nav navbar-right">
+        
+            <li style ="padding-top:2%; padding-bottom:1%">
+            <form method="post" action="customerHomeForEdits.php">
+                <input type="text" value="Search..." name="query" />
+                <input type="submit" value="Find" name="completedsearch" />
+            </form>
+            </li>
+
+            <li><a href="customerCart.php">Cart <span class="glyphicon glyphicon-shopping-cart"></span></a></li>
+            <li><a href="customerSettings.php">Settings <span class="glyphicon glyphicon-cog"></span></a></li>
+            <li><a href="customerHelp.php">Help <span class="glyphicon glyphicon-question-sign"></span></a></li>
+            <?php
+            if($_SESSION['Cemail']==""){
+                echo "<li><a href='login-customer.php'>Login<span class='glyphicon glyphicon-user'></span></a></li>";
+            }
+            else{
+                echo "<li><a href='customer-logout.php'>Logout:". $_SESSION['Cemail']."<span class='glyphicon glyphicon-user'></span></a></li>";
+            }
+            ?>
+            
         </ul>
         
-            <ul class="nav navbar-nav navbar-right">
-                <li style ="padding-top:2%; padding-bottom:1%"><input type="text" name="search" placeholder="Search.." style="border-radius:5px;"></li> 
-                <li><a href="customer-logout.php"><?php echo $_SESSION['email']; ?><span class="glyphicon glyphicon-user"></span></a></li>
-                <li><a href="customerCart.php">Cart <span class="glyphicon glyphicon-shopping-cart"></span></a></li>
-                <li><a href="customerSettings.php">Settings <span class="glyphicon glyphicon-cog"></span></a></li>
-                <li><a href="customerHelp.php">Help <span class="glyphicon glyphicon-question-sign"></span></a></li>
-            </ul>
-            <a class="btn btn-default" href="draftCustomerLogout.php" style="position:absolute; top:0; right:0;">Log Out <b></b></a>
+        
+        
+        
+        
+        
+        
+<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+    <ul class="nav navbar-nav">
 
-            
-            
+        <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Categories <span class="caret"></span></a>
+        <ul class="dropdown-menu">
+                <?php    
+            $storeID=$_SESSION['StoreID'];
+            $db = connectDB($DBHost, $DBUser, $DBPasswd, $DBName);
+            $query="SELECT * from categories where StoreID=$storeID order by catName;";
+            $result = queryDB($query, $db);
+            while($row = nextTuple($result)) {
+                $categoryID=$row['id'];
+                $catName=$row['catName'];
+    echo "<li class='dropdown-submenu'>";
+        echo "<a class='test' tabindex='-1' href='customerShowBigCategory.php?id=$categoryID'>$catName<span class='caret'></span></a>";
+        echo "<ul class ='dropdown-menu'>";
+        $query1="SELECT id,subName from SubCats where StoreID=$storeID and MainCatID=$categoryID;";
+        $result1 = queryDB($query1, $db);
+        echo  "<li><a tabindex='-1' href='customerShowBigCategory.php?id=$categoryID'>All $catName</a></li>";
+        while($row1 = nextTuple($result1)) {
+        $subName=$row1['subName'];
+        $subID=$row1['id'];
+                    
+            echo"<li><a tabindex='-1' href='customerItemsViewForEdits.php?id=$subID'>$subName</a></li>";
+        
+        }
+        echo"</ul>";
+        echo "</li>";
+        }
+            ?>
         </div>
+        
+        
+        
+        
+        
+    </div>
 
-            
-    </nav>
+        
+</nav>
     <!--we have the Nav, now the cool stuff starts check out AS GUEST-->
     <!--bread crumb place-->
 
