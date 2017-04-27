20f86462e7a532ed8243bbd1ae1606b642074161
chbassplayer <jessica-lu@uiowa.edu>
2017-04-26 20:29:19 -0500

    test1

--- categories.sql
+++ categories.sql
@@ -4,23 +4,27 @@ DROP TABLE IF EXISTS SubCats;
 CREATE TABLE categories (
     id INT NOT NULL AUTO_INCREMENT,
     catName VARCHAR(40) NOT NULL,
-    PRIMARY KEY (id)
+    StoreID int NOT NULL,--remember to take this out
+    PRIMARY KEY (id,StoreID)
 );
 
 CREATE TABLE SubCats(
     id INT NOT NULL AUTO_INCREMENT,
     subName VARCHAR(40) NOT NULL,
     MainCatID INT NOT NULL,
+    StoreID int NOT NULL,
     Primary KEY(id)
 );
 
 
-INSERT INTO categories (catName)VALUES ('Dairy');
-INSERT INTO categories (catName)VALUES ('Produce');
-INSERT INTO categories (catName)VALUES ('Cleaning');
-INSERT INTO categories (catName)VALUES ('Frozen');
+INSERT INTO categories (catName,StoreID)VALUES ('Dairy',1);
+INSERT INTO categories (catName,StoreID)VALUES ('Produce',1);
+INSERT INTO categories (catName,StoreID)VALUES ('Cleaning',1);
+INSERT INTO categories (catName,StoreID)VALUES ('Frozen',1);
+INSERT INTO categories (catName,StoreID)VALUES ('Frozen2',2);
 
-INSERT INTO SubCats(subName,MainCatID)VALUES('Yogurt',1);
-INSERT INTO SubCats(subName,MainCatID)Values('Vegetable',2);
-INSERT INTO SubCats(subName,MainCatID)Values('Carpet Cleaning',3);
-INSERT INTO SubCats(subName,MainCatID)Values('Pizza',4);
+INSERT INTO SubCats(subName,MainCatID,StoreID)VALUES('Yogurt',1,1);
+INSERT INTO SubCats(subName,MainCatID,StoreID)Values('Vegetable',2,1);
+INSERT INTO SubCats(subName,MainCatID,StoreID)Values('Carpet Cleaning',3,1);
+INSERT INTO SubCats(subName,MainCatID,StoreID)Values('Pizza',4,1);
+INSERT INTO SubCats(subName,MainCatID,StoreID)Values('Pizza2',4,2);
